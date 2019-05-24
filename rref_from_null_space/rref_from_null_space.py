a62bab53c91d3e8a7b41ca222635fcf9a26e89b4
cfiumara <34575268+cfiumara@users.noreply.github.com>
2019-05-23 23:47:46 -0700

    Address incorrect typing and exact values

--- rref_from_null_space/rref_from_null_space.py
+++ rref_from_null_space/rref_from_null_space.py
@@ -1,36 +1,60 @@
-# rref_from_null_space is designed to take a given null space, reduce it if needed,
+# rref_from_null_space is (poorly) designed to take a given null space, reduce it if needed,
 # and from the reduced null space, to determine the RREF of the matrix that produced
 # the given null space.
 
-from __future__ import division # not sure if necessary
 from sympy import *
 
 def main():
 
-    # Acquire null space, null_given, from user (for testing, working with hardcoded one)
+    # Acquire amount of columns in the null space, each column's contents, and number of rows desired in target matrix
 
-    null_given = 0
+    null_columns = int(input("Enter the number of columns in the given null space:  "))
+    rows_desired = int(input("Enter the desired number of rows in the target matrix:  "))
+    null_given = []
+
+    print("\nTo enter square roots, use the format sqrt(n).")
+    print("To enter other roots or exponents, use the format x**(a/b).")
+    print("To enter rational numbers, use the format n/m.\n")
+
+    for i in range(0, null_columns):
+
+        if i == 0:
+
+            null_column = input("Enter first column of null space, separating entries with only spaces:  ")
+
+        else:
+
+            null_column = input("Enter next column of null space, separating entries with only spaces:  ")
+
+        null_column_str = null_column.split()
+
+        null_column_converted = []
+
+        for i in range(0, len(null_column_str)):
+
+            null_column_converted.append(sympify(null_column_str[i]))
+
+        null_given.append(null_column_converted)
 
     # Put the null space into a matrix A.
 
-    A = Matrix([[1, 2, 3, 4], [5, 6, 7, 8]])
+    for i in range(0, null_columns):
+
+        if i == 0:
+
+            A = Matrix(null_given[i]).T
+
+        else:
+            
+            A = A.row_insert(i, Matrix(null_given[i]).T)
 
     # Put A into RREF. Assign it to rref_A.
 
     rref_A = A.rref()
 
-    pprint(A.rref())
-    pprint(A.nullspace())
-
     # Determine the null space of the matrix A and assign it to a null_A.
 
     null_A = A.nullspace()
-
-    print("null_A is \n")
-    pprint(null_A)
-
-    print("len(null_A[0]) is \n")
-    pprint(len(null_A[0]))
 
     # Initialize a matrix B. Make B have the right amount of columns first.
 
@@ -40,36 +64,50 @@ def main():
 
         B = B.col_insert(i, Matrix([0]))
 
-    print("B after resizing is: \n")
-    pprint(B)
-
     # Put null_A into a matrix B. Working on this section right now.
 
     row = []
 
-    for i in range(0, len(null_A[0])):
+    for i in range(0, len(null_A)):
 
-        row.append(null_A[otheriteratorneeded[i]])
+        row.clear()
 
-    print("\n\n Row to be inserted:\n")
+        for j in range(0, len(null_A[0])):
 
-    pprint(row)
+            row.append(null_A[i][j])
 
-    print("\n\n Inserting row... \n\n")
+        B = B.row_insert(i, Matrix([row]))
 
-    B = B.row_insert(i, Matrix([row]))
+        if i == 0:
 
-    #pprint("\nB is: \n", B)
-    #pprint("\n")
+            B.row_del(1)
+
+    # Fill B with rows of zeroes, as needed, to fulfill the desired amount of rows.
+
+    B_dimensions = B.shape
+
+    if B_dimensions[0] < rows_desired:        
+
+        rows_to_add = rows_desired - B_dimensions[0]
+        
+        #  Make a row of all zeroes to fill out the rest of the matrix
+
+        adding = zeros(1, B_dimensions[1])
+
+        for i in range(0, rows_to_add):
+
+            B = B.row_insert(B_dimensions[0], adding)
 
     # Put B in RREF, and assign it to rref_B.
-    
+
     rref_B = B.rref()
 
-    # Return rref_B, as this is the matrix of the original matrix.
+    # Print for display until I'm no longer too lazy to clean up this code.
 
-    pprint(rref_B)
+    pprint(rref_B[0])
 
-    return rref_B
+    # Return rref_B's matrix component, as this is also the original matrix.
+
+    return rref_B[0]
 
 main()
